f5cb58d47cac4a12ad627bd27c66b26883dcb38c
Allester Corton <cortonallester@gmail.com>
2024-06-01 22:32:51 +0800

    Updated Resume and Display

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,66 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('EverDev');
-    
-    // header scroll event
-    const header = document.querySelector('.header');
+  // console.log('EverDev')
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 0) {
-            header.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
-        }
-    });
+  // header scroll event
+  const header = document.querySelector('.header')
 
-    // Menu bar toggle button
-    const menu = document.querySelector('.menu');
-    const hiddenNav = document.querySelector('.hidden-nav');
-    const exitButton = hiddenNav.querySelector('.exit-button');
-    const newNavLinks = hiddenNav.querySelectorAll('a');
-    let isOpen = false;
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled')
+    } else {
+      header.classList.remove('scrolled')
+    }
+  })
 
-    menu.addEventListener('click', () => {
-        toggleHiddenNav();
-    });
+  // Menu bar toggle button
+  const menu = document.querySelector('.menu')
+  const hiddenNav = document.querySelector('.hidden-nav')
+  const exitButton = hiddenNav.querySelector('.exit-button')
+  const newNavLinks = hiddenNav.querySelectorAll('a')
+  let isOpen = false
 
-    exitButton.addEventListener('click', () => {
-        slideOutHiddenNav();
-    });
+  menu.addEventListener('click', () => {
+    toggleHiddenNav()
+  })
 
-    newNavLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            toggleHiddenNav();
-        });
-    });
+  exitButton.addEventListener('click', () => {
+    slideOutHiddenNav()
+  })
 
-    function toggleHiddenNav() {
-        isOpen = !isOpen;
-        if (isOpen) {
-            hiddenNav.style.display = 'block';
-            hiddenNav.classList.add('slide-in');
-        } else {
-            slideOutHiddenNav();
-        }
-    };
+  newNavLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      toggleHiddenNav()
+    })
+  })
 
-    function slideOutHiddenNav() {
-        hiddenNav.classList.add('slide-out');
-        hiddenNav.addEventListener('animationend', () => {
-            hiddenNav.style.display = 'none';
-            hiddenNav.classList.remove('slide-out');
-        }, { once: true });
-        isOpen = false;
-    };
+  function toggleHiddenNav() {
+    isOpen = !isOpen
+    if (isOpen) {
+      hiddenNav.style.display = 'block'
+      hiddenNav.classList.add('slide-in')
+    } else {
+      slideOutHiddenNav()
+    }
+  }
 
-    // download Resume
-    const downloadCv = document.querySelector('#download-cv');
+  function slideOutHiddenNav() {
+    hiddenNav.classList.add('slide-out')
+    hiddenNav.addEventListener(
+      'animationend',
+      () => {
+        hiddenNav.style.display = 'none'
+        hiddenNav.classList.remove('slide-out')
+      },
+      { once: true }
+    )
+    isOpen = false
+  }
 
-    downloadCv.addEventListener('click', () => {
-        const link = document.createElement('a');
-        link.href = 'assets/pdf/Corton-Resume.pdf'; // path of the Resume file
-        link.download = 'Allester-Corton-Resume.pdf'; // desired file name for download
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    });
-});
+  // download Resume
+  const downloadCv = document.querySelector('#download-cv')
+
+  downloadCv.addEventListener('click', () => {
+    const link = document.createElement('a')
+    link.href = 'assets/pdf/Allester_Corton_Resume.pdf' // path of the Resume file
+    link.download = 'Allester_Corton_Resume.pdf' // desired file name for download
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  })
+})
